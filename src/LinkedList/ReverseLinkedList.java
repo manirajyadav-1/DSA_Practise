3b17a88d4df8cfe23753adb8bdf83d9c7535b996
manirajyadav-1 <yadavmani41@gmail.com>
2024-09-09 22:35:31 +0530

    Added ReverseLinkedList Problem

---
+++ src/LinkedList/ReverseLinkedList.java
@@ -0,0 +1,56 @@
+package LinkedList;
+import java.util.*;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+public class ReverseLinkedList {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode next = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    public void printList(ListNode head) {
+        ListNode curr = head;
+        while (curr != null) {
+            System.out.print(curr.val + " ");
+            curr = curr.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        ReverseLinkedList list = new ReverseLinkedList();
+
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        System.out.println("Original List:");
+        list.printList(head);
+
+        // Reverse the linked list
+        head = list.reverseList(head);
+
+        System.out.println("Reversed List:");
+        list.printList(head);
+    }
+}
