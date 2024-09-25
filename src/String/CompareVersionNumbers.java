f0987185447615807e050d6c6bfb33508bbaa015
manirajyadav-1 <yadavmani41@gmail.com>
2024-09-25 22:25:04 +0530

    Added CompareVersionNumbers

---
+++ src/String/CompareVersionNumbers.java
@@ -0,0 +1,34 @@
+package String;
+
+public class CompareVersionNumbers {
+    public static int compareVersion(String version1, String version2){
+        int i = 0, j = 0;
+
+        while(i < version1.length() || j < version2.length()){
+            int num1 = 0, num2 = 0;
+
+            while(i < version1.length() && version1.charAt(i) != '.'){
+                num1 = num1 * 10 + (version1.charAt(i++) - '0');
+            }
+
+            while(j <version2.length() && version2.charAt(j) != '.'){
+                num2 = num2 * 10 + (version2.charAt(j++) - '0');
+            }
+
+            if(num1 < num2){
+                return -1;
+            }
+            if(num1 > num2) {
+                return 1;
+            }
+            i++;
+            j++;
+        }
+        return 0;
+    }
+    public static void main(String[] args) {
+        String version1 = "1.2";
+        String version2 = "1.10";
+        System.out.println(compareVersion(version1, version2));
+    }
+}
