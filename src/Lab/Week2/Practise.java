80dfba4eafc41d181d63ae07d930641ce530d88a
manirajyadav-1 <yadavmani41@gmail.com>
2025-03-14 15:32:27 +0530

    Added new program

--- src/Lab/Week2/Practise.java
+++ src/Lab/Week2/Practise.java
@@ -1,5 +1,13 @@
-class Practise{
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+
+class Practise{
+    public static void main(String[] args) throws IOException{
+        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = r.readLine();
+        System.out.println(s);
     }
 }
