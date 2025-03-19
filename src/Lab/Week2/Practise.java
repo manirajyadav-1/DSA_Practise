dfc8b1f12567a26f35d4f22f0b9e45c8cdeec194
manirajyadav-1 <yadavmani41@gmail.com>
2025-03-19 21:54:17 +0530

    Added new program

--- src/Lab/Week2/Practise.java
+++ src/Lab/Week2/Practise.java
@@ -1,13 +1,23 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 
 
 class Practise{
-    public static void main(String[] args) throws IOException{
-        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
 
-        String s = r.readLine();
-        System.out.println(s);
+    public static void main(String[] args){
+        int arr[] = {9,6,4,3,5};
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int num : arr){
+            map.put(num, map.getOrDefault(num, 0)+1);
+        }
+
+        for(int num : map.keySet()){
+            System.out.println(num);
+        }
+
+        for(Map.Entry entry : map.entrySet()){
+            System.out.println(entry.getKey());
+        }
     }
 }
